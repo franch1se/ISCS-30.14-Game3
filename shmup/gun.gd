8d9e8726a3d384cd744799c4c8b0312d5535eff6
AdrMrcs <adrian.villanueva@student.ateneo.edu>
2024-10-06 16:46:54 +0800

    added sfx

--- shmup/gun.gd
+++ shmup/gun.gd
@@ -2,14 +2,15 @@ extends Node2D
 
 @export var bullet_scene : PackedScene = load("res://player_bullet.tscn")
 @onready var player = $"../CharacterBody2D"
+@onready var laserSFX = $"../laserSFX"
 
 var FIRE_RATE := 50.0
 
-func _process(delta: float) -> void:
-	if Input.is_action_pressed("shoot"):
-		var bullet_count = round(FIRE_RATE * delta)
-		for index in bullet_count:
-			var bullet = bullet_scene.instantiate()
-			bullet.position = player.position
-			bullet.rotation = player.rotation
-			add_child(bullet)
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("shoot"):
+		var bullet = bullet_scene.instantiate()
+		bullet.position = player.position
+		bullet.rotation = player.rotation
+		add_child(bullet)
+		laserSFX.play()
