1e0e9fec69e964836fe82682b8ef4d7260ef0c2f
Bryan <franciscobryaaan@gmail.com>
2024-10-06 12:28:31 +0800

    better bullet interaction

--- shmup/enemy_1.gd
+++ shmup/enemy_1.gd
@@ -1,7 +1,5 @@
-extends Node2D
+extends Area2D
 
-
-@export var impact_scene : PackedScene = load("res://bullet_impact.tscn")
 
 var hp := 50
 
@@ -10,24 +8,10 @@ func _ready() -> void:
 
 
 func _physics_process(delta: float) -> void:
+	position.x -= delta*5 # to be deleted
+	
 	# Process hp
 	if hp <= 0:
-		# do some animations
 		queue_free()
 	
 	# Process movement
-	
-func _on_hitbox_area_entered(area: Area2D) -> void:
-	if area.is_in_group("player_bullet"):
-		#print("Enemy hp: ", hp)
-		hp -= 1
-		var impact = impact_scene.instantiate()
-		impact.position = area.get_child(0).position
-		#print("AREA POS: ", area.get_child(0).position)
-		add_child(impact)
-		area.queue_free()
-		
-
-func _on_hitbox_body_entered(body: Node2D) -> void:
-	hp -= 1
-	
--- shmup/player_bullet.gd
+++ shmup/player_bullet.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+@export var impact_scene : PackedScene = load("res://bullet_impact.tscn")
 
 var SPEED := 1750.0
 
@@ -7,8 +8,15 @@ func _physics_process(delta: float) -> void:
 	var distance := SPEED * delta
 	var motion := -transform.y * SPEED * delta
 	position += motion
-	print("BUL POS: ", position)
-
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
+
+func _on_area_entered(area: Area2D) -> void:
+	if area.is_in_group("enemy"):
+		var impact = impact_scene.instantiate()
+		impact.global_position = global_position
+		impact.rotation = rotation - deg_to_rad(20)
+		get_parent().add_child(impact)
+		area.hp -= 1
+		queue_free()
